f5d3d9afee3e44c48d61e3cbd02c0b17122864b0
wkumler <wkumler@uw.edu>
2020-09-12 15:37:58 -0700

    Established ui, server, function setups

---
+++ app.R
@@ -0,0 +1,24 @@
+
+
+# Setup things ----
+
+
+
+# Functions ----
+
+
+
+# UI ----
+ui <- navbarPage()
+
+
+
+# Server ----
+server <- function(input, output, session){
+  
+}
+
+
+
+# Run app ----
+shinyApp(ui = ui, server = server)
