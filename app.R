e013288e98c9dd68ebc9afaf29f20386042bbeb0
wkumler <wkumler@uw.edu>
2020-09-13 10:13:48 -0700

    Added age information

--- app.R
+++ app.R
@@ -20,7 +20,7 @@ library(jpeg)
 
 # UI ----
 ui <- navbarPage(
-  title = "Back again?",
+  title = "Dash_R",
   tabPanel(
     titlePanel("Home"),
     mainPanel(
@@ -33,6 +33,23 @@ ui <- navbarPage(
                  gsub(pattern = "^0", replacement = "", x = .) %>%
                  paste0("It's ", .) %>%
                  HTML() %>%
+                 h2(),
+               br(),
+               Sys.time() %>%
+                 as.POSIXct() %>%
+                 `attr<-`("tzone", "America/Los_Angeles") %>%
+                 `-`(strptime("1997-03-28", "%Y-%m-%d")) %>%
+                 round() %>%
+                 paste("You're", ., "days old today!") %>%
+                 h3(),
+               Sys.time() %>%
+                 as.POSIXct() %>%
+                 `attr<-`("tzone", "America/Los_Angeles") %>%
+                 `-`(strptime("1997-03-28", "%Y-%m-%d")) %>%
+                 as.numeric() %>%
+                 `*`(24) %>%
+                 round() %>%
+                 paste0("(", ., " hours)") %>%
                  h3()
         )
       )
